40b805debf6e8d3191feb6cbfeb8b1e63969c9f0
Лилит <@LAPTOP-3EJS7JBS>
2023-07-02 11:03:50 +0300

    Добавьте файлы проекта.

---
+++ HW_tema20_3/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Write("Введите первое число: ");
+int x = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int y = int.Parse(Console.ReadLine()!);
+
+My_foo(x, y);
+void My_foo(int a, int b)
+{
+    Console.Write("Введите операцию: ");
+    string operation = Console.ReadLine()!;
+
+    int result=0;
+
+    switch (operation)
+    {
+        case "&":
+            result =a & b;
+            break;
+
+        case "|":
+            result = a | b;
+            break;
+
+        case "^":
+            result = a ^ b;
+            break;
+
+        case "~":
+            result = ~a;
+            break;
+
+        case "<<":
+            result = a << b;
+            break;
+
+        case ">>":
+            result = a >> b;
+            break;
+
+        default:
+            Console.WriteLine("Error");
+            break;
+    }
+    Console.WriteLine(result);
+}
